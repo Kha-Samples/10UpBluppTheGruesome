2efbf307871a5ae5c478e175779de2b1e562e994
Ingo Jakobs <ingo.jakobs@stud.tu-darmstadt.de>
2015-08-22 20:59:47 +0200

    added computer.png

--- Sources/sprites/Computer.hx
+++ Sources/sprites/Computer.hx
@@ -11,7 +11,7 @@ class Computer extends InteractiveSprite {
 	var idLogger: IdLogger = new IdLogger(Keys_text.COMPUTER);
 	
 	public function new(x: Float, y: Float ) {
-		super(Loader.the.getImage("0"), 46 * 2, 60 * 2, 0);
+		super(Loader.the.getImage("computer"), 46 * 2, 60 * 2, 0);
 		this.x = x;
 		this.y = y;
 		
