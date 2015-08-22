f8e38b645d165426865f12adba10a1ef5b21848c
Ingo Jakobs <ingo.jakobs@stud.tu-darmstadt.de>
2015-08-22 19:27:16 +0200

    added id logger to Computer

--- Sources/sprites/Computer.hx
+++ Sources/sprites/Computer.hx
@@ -1,12 +1,30 @@
 package sprites;
 
 import kha.Loader;
+import kha2d.Direction;
 import kha2d.Sprite;
+import localization.Keys_text;
 
-class Computer extends Sprite {
+import sprites.IdSystem;
+
+class Computer extends InteractiveSprite {
+	var idLogger: IdLogger = new IdLogger(Keys_text.COMPUTER);
+	
 	public function new(x: Float, y: Float ) {
 		super(Loader.the.getImage("0"), 46 * 2, 60 * 2, 0);
 		this.x = x;
 		this.y = y;
-	}	
+		
+		this.isUseable = true;
+	}
+	
+	override public function useFrom(dir:Direction, user:Dynamic) 
+	{
+		if (Std.is(user, sprites.IdCardOwner))
+		{
+			var owner: IdCardOwner = cast user;
+			
+			idLogger.useID(owner.IdCard);
+		}
+	}
 }
