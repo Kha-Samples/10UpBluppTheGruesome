b23b321f842c3709d8642a94ddfef2eada56eeda
Ingo Jakobs <ingo.jakobs@stud.tu-darmstadt.de>
2015-08-23 22:36:46 +0200

    fixes to player change with open Dialogues

--- Sources/Empty.hx
+++ Sources/Empty.hx
@@ -284,6 +284,7 @@ class Empty extends Game {
 		}
 		player.setCurrent();
 		Scene.the.addHero(player);
+		nextDayChangeTime = Scheduler.time() + 60.0;
 	}
 	
 	private static function isCollidable(tilenumber: Int): Bool {
@@ -340,7 +341,7 @@ class Empty extends Game {
 			if (Scheduler.time() >= nextDayChangeTime)
 			{
 				isDay = !isDay;
-				nextDayChangeTime = Scheduler.time() + 60.0;
+				nextDayChangeTime = Math.NaN;
 				if (isDay) Dialogues.dawn();
 				else Dialogues.dusk();
 			}
