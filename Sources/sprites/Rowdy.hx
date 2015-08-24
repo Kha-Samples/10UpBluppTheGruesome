6bb91a4776be890dc17f9459946f4c9409ac793f
Robert Konrad <robert@ktx-software.com>
2015-08-24 16:10:30 +0200

    Added the remaining members of the 10Up team.

---
+++ Sources/sprites/Rowdy.hx
@@ -0,0 +1,17 @@
+package sprites;
+
+import kha.Loader;
+import kha2d.Animation;
+import sprites.IdSystem.IdCard;
+
+class Rowdy extends RandomGuy {
+	public function new(stuff: Array<InteractiveSprite>, youarethemonster: Bool) {
+		super(stuff, youarethemonster);
+		image = Loader.the.getImage("rowdy");
+		standLeft = Animation.create(10);
+		standRight = Animation.create(0);
+		walkLeft = Animation.createRange(11, 18, 4);
+		walkRight = Animation.createRange(1, 8, 4);
+		IdCard = new IdCard("Rowdy");
+	}
+}
